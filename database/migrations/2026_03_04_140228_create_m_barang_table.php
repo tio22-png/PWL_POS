dd670288a8264a3f185cacb56033035b454d758f
tio <hanggoronursetio@gmail.com>
2026-03-04 21:22:46 +0700

    Praktikum 2

---
+++ database/migrations/2026_03_04_140228_create_m_barang_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_barang', function (Blueprint $table) {
+            $table->id('barang_id');
+            $table->unsignedBigInteger('kategori_id')->index(); // indexing untuk ForeignKey
+            $table->string('barang_kode', 10)->unique();
+            $table->string('barang_nama', 100);
+            $table->decimal('harga_beli', 15, 2);
+            $table->decimal('harga_jual', 15, 2);
+            $table->integer('stok')->default(0);
+            $table->timestamps();
+
+            // Mendefinisikan Foreign Key
+            $table->foreign('kategori_id')->references('kategori_id')->on('m_kategori');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_barang');
+    }
+};
