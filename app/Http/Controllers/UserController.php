86d253c2e29e3bce5821fe764d9de9a6fd8a7962
tio <hanggoronursetio@gmail.com>
2026-03-05 10:26:08 +0700

    Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,18 +9,9 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'Manager_3',
-            'nama' => 'Manager 3',
-            'password' => ('12345')
-        ];
-        UserModel::create($data); // create data user
-
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        $user = UserModel::findOr(20, ['username','nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
-
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,27 +7,19 @@
 </head>
 <body>
     <h1>Data User</h1>
-    <table border="1" cellpadding="5" cellspacing="0">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>Level</th>
-                <th>Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($data as $row)
-                <tr>
-                    <td>{{ $row->user_id }}</td>
-                    <td>{{ $row->username }}</td>
-                    <td>{{ $row->nama }}</td>
-                    <td>{{ $row->level_id }}</td>
-                    <td>{{ $row->status }}</td>
-                </tr>
-            @endforeach
-        </tbody>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+        </tr>
+        <tr>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
+        </tr>
     </table>
 </body>
 </html>
